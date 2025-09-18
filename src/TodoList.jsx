08b05181bd145bf6e1a4d44fd6945ecfa15aa0da
Manoj-Gmk <manojgare528@gmail.com>
2025-09-18 18:47:38 +0530

    Update TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 
-// TodoList component receives todos, handledelete, and handleEdit as props
 function TodoList({ todos, handledelete, handleEdit }) {
   return (
     <ol>
-      {/* Map through each todo and display it in a list item */}
       {todos.map((todo, index) => (
         <li key={index}>
-          {/* Display the task text */}
           <span className="text">{todo}</span>
-
-          {/* Edit button to modify the selected task */}
           <button className="editbutton" onClick={() => handleEdit(index)}>
             Edit
           </button>
-
-          {/* Delete button to remove the selected task */}
           <button className="deletebutton" onClick={() => handledelete(index)}>
             Delete
           </button>
